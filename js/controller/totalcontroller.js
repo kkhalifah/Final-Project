7d6e0ce931b2afdcc21542eed738630e56a3ed91
kkhalifah <karen.khalifah@gmail.com>
2017-05-28 15:10:39 -0400

    controllers are connected

--- js/controller/totalcontroller.js
+++ js/controller/totalcontroller.js
@@ -1,8 +1,22 @@
 
 var app = angular.module('swearApp');
 //probably don't need http for this Ctlr
-app.controller('totalController', function ($scope, $http, $location){
+app.controller('totalController', function ($scope, $location){
   console.log("total controller is connected");
+
+  var results = "";
+  $scope.testForTrue = function(results){
+    if (results === true) {
+      add 1 to the total count
+      console.log(num + "words are true");
+    } else (results === false) {
+        return no number added
+      console.log("word is false");
+
+    }
+
+  };
+
 
 
 
---
+++ js/service/service.js
@@ -0,0 +1,27 @@
+//to be set up at some point
+
+var app = angular.module("swearApp");
+
+app.factory('totalFactory', function(){
+  console.log("hi factory");
+var input = "aword";
+return {
+
+  setInput:  function (word){
+    if (word === true) {
+      input = increment 1;
+      console.log(increment by 1);
+    }
+
+    else (word === false){
+      return increment 0
+      retutn nothing
+    }
+
+},
+  getInput: function (totalWordsConsoled) {
+    console.log(totalWordsConsoled);
+    return currency amount
+  }
+}
+});
