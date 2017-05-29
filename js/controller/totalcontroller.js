28f06f343fe967acb1b8d30c0094f98c497c7a66
kkhalifah <karen.khalifah@gmail.com>
2017-05-29 11:50:56 -0400

    connect factory

--- js/controller/totalcontroller.js
+++ js/controller/totalcontroller.js
@@ -3,15 +3,17 @@ var app = angular.module('swearApp');
 //probably don't need http for this Ctlr
 app.controller('totalController', function ($scope, $location){
   console.log("total controller is connected");
+
+
 //log total true statements on this page
 //scope values from view one here {{totals}}
-  var booleans = ['true', 'false'];
+//  var booleans = ['true', 'false'];
   //word count plus 1
-  var numtotal = 4;
-  $scope.testForTrue = function (boolean){
+//  var numtotal = 4;
+//  $scope.testForTrue = function (boolean){
 
-    console.log($scope.boolean);
-  }
+  //  console.log($scope.boolean);
+//  }
   //it works!
   $scope.backButton = function (){
     $location.path('/index')
--- js/service/service.js
+++ js/service/service.js
@@ -1,27 +1,32 @@
 //to be set up at some point
-
 var app = angular.module("swearApp");
-
+//needs to be connected
 app.factory('totalFactory', function(){
   console.log("hi factory");
-var input = "aword";
-return {
 
-  setInput:  function (word){
-    if (word === true) {
-      input = increment 1;
-      console.log(increment by 1);
-    }
+//take info from view 1 and display on page two from submit button
+//display which radio button is chosen to console
+//display if word is true or false
+//log count of times true is used
+//display to page using ng-expression
 
-    else (word === false){
-      return increment 0
-      retutn nothing
-    }
 
-},
-  getInput: function (totalWordsConsoled) {
-    console.log(totalWordsConsoled);
-    return currency amount
-  }
-}
+
+
+
+//display image to change depending on value
+
+// psuedo code idea one
+// var input = "aword";
+// return {
+//   setInput:  function (word){
+//     if (word === true) {
+//       input = increment 1;
+//       console.log(increment by 1);}
+//     else (word === false){
+//       return increment 0
+//       retutn nothing}},
+//   getInput: function (totalWordsConsoled) {
+//     console.log(totalWordsConsoled);
+//     return currency amount}}
 });
